9c4963aba29c0eff641ee5e0c9f7d1e84a05416d
Wei Lu <luwei.here@gmail.com>
2012-01-14 00:11:31 +0800

    project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cuteoverflow::Application.config.secret_token = '4f008c5e33a62db9cebc9a6186b696edbb76be88ff58e6be4c8f2867d304659d2cb8d2323646c742fb69bf28c34569b521d78ff2153447f94e6c934219613aa4'
